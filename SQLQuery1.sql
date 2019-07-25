404138cc842aad48bdefc9c92dc7cfd5851ddf7f
AbirajTimalsina <49244468+AbirajTimalsina@users.noreply.github.com>
2019-07-26 00:29:00 +0545

    latest upload
    latest upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -121,7 +121,6 @@ insert into conference_organization_Abiraj(conference_organization_name) values
 ('African History Society'),
 ('The Victorian History Society'),
 ('Early Railways Group');
-
 select * from conference_organization_Abiraj
 
 insert into conference_Abiraj(conference_id,conference_name,date_start,date_end,conference_organization_id, venue_id) values
@@ -134,15 +133,14 @@ insert into conference_Abiraj(conference_id,conference_name,date_start,date_end,
 ('VSC18','Victorian Studies Conference','2018-01-03','2018-01-04', 6, 'QVR'),
 ('ERG18','Early Railways Conference','2018-01-19','2018-01-21', 7, 'OXB');
 
-
 select * from conference_Abiraj
 -- END--------------------------------------------------
 
 ---------------Task 2C-----------------------
+
 insert into room_Abiraj(room_name, venue_id) values
 ('Manston Hall','QVR'),
 ('Robin Hall','QVR');
-
 select * from room_Abiraj
 
 
@@ -153,7 +151,6 @@ insert into speaker_Abiraj(speaker_firstname, speaker_lastname) values
 ('Mark','Reynolds'),
 ('Abel','Kelani'),
 ('Albert','Dean');
-
 select * from speaker_Abiraj
 
 
@@ -184,3 +181,82 @@ insert into facility_Abiraj(facility_name) values
 select *from facility_Abiraj
 
 
+insert into venue_facility_Abiraj(venue_ID, facilityId) values
+('UCL','1'),('UCL','2'),('UCL','3'),('UCL','4'),('UCL','5'),
+('MMU','1'),('MMU','2'),('MMU','3'),('MMU','4'),('MMU','5'),
+('ARU','2'),('ARU','3'),
+('BH','2'),('BH','3'),
+('KCL','2'),
+('OXB','1'),('OXB','2'),('OXB','3'),('OXB','4'),('OXB','5');
+
+select * from venue_facility_Abiraj
+
+----------#END#---------------------------------
+
+--------------2E-----------------------------
+
+insert into equipment_Abiraj(equipment_name) values
+('Projecter'),
+('WIFI'),
+('Smart Board'),
+('PC'),
+('PA System');
+
+select * from equipment_Abiraj
+
+insert into room_equipment_Abiraj(room_id, equipmentId) values
+('1','3'),('1','4'),('1','5'),
+('2','1'),('2','4'),('2','5');
+
+select * from room_equipment_Abiraj
+--------#END#-----------------------
+
+---------------------2F----------
+select c.conference_ID, co.conference_organization_Name, l.location_Name from conference_Abiraj c 
+inner join venue_Abiraj v on c.venue_id=v.venue_id inner join location_Abiraj l on v.location_id=l.locationID 
+inner join conference_organization_Abiraj co on c.conference_organization_id=co.conference_organization_id 
+and l.location_name='Manchester'
+
+--END-----------------
+------2G--------------
+select c.date_start, l.location_name from conference_Abiraj c 
+inner join venue_Abiraj v on c.venue_id=v.venue_id inner join location_Abiraj l on v.location_id=l.locationID 
+and l.location_name='London'
+
+----END-------------------------
+
+-------------------2H---------------------
+
+select crs.speaker_date, crs.slot, r.room_name, (s.speaker_firstname+' '+s.speaker_lastname)as Name, crs.topic 
+from conference_room_speaker_Abiraj crs inner join conference_Abiraj c on crs.conference_id=c.conference_id
+inner join room_Abiraj r on crs.room_id=r.room_id 
+inner join speaker_Abiraj s on crs.speaker_id=s.speaker_id and c.conference_id='VSC18'
+
+-----END--------------------
+
+------------------------2I------------------
+
+select equipment_name, room_name from equipment_Abiraj e, room_Abiraj r, room_equipment_Abiraj re 
+where (e.equipmentId=re.equipmentId AND r.room_id=re.room_id) and r.room_name='Manston Hall'    
+
+-----END--------------------
+
+------------------------2J------------------
+
+select facility_name, venue_name from venue_Abiraj v, facility_Abiraj f, venue_facility_Abiraj vf 
+where (v.venue_id=vf.venue_id AND f.facilityId=vf.facilityId) and v.venue_name='University College London'    
+
+-----END--------------------
+
+------------------------2K------------------
+
+insert into speaker_Abiraj(speaker_firstname,speaker_lastname) values('Robert','Matthews');
+
+update conference_room_speaker_Abiraj set speaker_id=6 from speaker_Abiraj s
+inner join conference_room_speaker_Abiraj crs on s.speaker_Id=crs.speaker_ID
+inner join room_Abiraj r on r.room_id=crs.room_id where (r.room_name='Manston Hall' and crs.slot='AM') 
+and crs.speaker_date='2018-01-03'
+
+select * from speaker_Abiraj 
+select * from conference_room_speaker_Abiraj
+-----END--------------------
